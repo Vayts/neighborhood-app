0f712ed3c86f0a269334091fe286cc34b45f5762
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-04-29 19:36:18 +0300

    add Search.tsx

---
+++ client/src/components/UI/Search/style.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { ISearchStyle } from '@src/components/UI/Search/types';
+
+export const SearchWrapper = styled.label<ISearchStyle>`
+  margin: ${({ margin = '5px 0' }) => margin};
+  width: ${({ width = 'auto' }) => width};
+  font-size: ${({ fz = 16 }) => `${fz}px`};
+  height: ${({ height = 'auto' }) => height};
+  position: relative;
+	display: inline-block;
+`;
+
+export const SearchInput = styled.input`
+  height: 100%;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 10px 10px 10px 40px;
+  border-radius: 10px;
+  border: 1px solid #ECECED;
+  background-color: #f3f3f3;
+	
+	&::placeholder {
+    font-weight: 600;
+	}
+`;
+
+export const SearchIcon = styled.span`
+  position: absolute;
+	left: 10px;
+	top: 50%;
+	transform: translateY(-50%);
+	font-size: 20px;
+	color: #4F4B5C;
+`;
