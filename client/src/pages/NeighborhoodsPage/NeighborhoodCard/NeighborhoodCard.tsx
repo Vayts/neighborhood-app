ff0235b3e0067e292e7e371da43c0a6de3ed4133
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-04-29 13:37:05 +0300

    add client and server logic for user neighborhoods

--- client/src/pages/NeighborhoodsPage/NeighborhoodCard/NeighborhoodCard.tsx
+++ client/src/pages/NeighborhoodsPage/NeighborhoodCard/NeighborhoodCard.tsx
@@ -8,16 +8,17 @@ import { Title } from '@src/components/UI/Title/Title';
 import { Description } from '@src/components/UI/Description/Description';
 import { UserList } from '@src/components/UI/UserList/UserList';
 import { AvatarFiller } from '@src/components/UI/AvatarFiller/AvatarFiller';
+import { INeighborhoodCard } from '@src/pages/NeighborhoodsPage/NeighborhoodCard/types';
 
-export const NeighborhoodCard: React.FC = () => {
+export const NeighborhoodCard: React.FC<INeighborhoodCard> = ({ neighborhood }) => {
 	return (
 		<NeighborhoodCardWrapper>
 			<NeighborhoodCardTopContent>
-				<AvatarFiller text='Test Title' size={45}/>
-				<UserList users={[{ login: 'xiba', _id: 'asd', firstName: 'asdasd', lastName: 'asdasd', avatar: null }, { login: 'biba', _id: 'asd', firstName: 'asdasd', lastName: 'asdasd', avatar: null }, { login: 'yiba', _id: 'asd', firstName: 'asdasd', lastName: 'asdasd', avatar: null }, { login: 'biba', _id: 'asd', firstName: 'asdasd', lastName: 'asdasd', avatar: null }]}/>
+				<AvatarFiller text={neighborhood.title} size={45}/>
+				<UserList users={neighborhood.members}/>
 			</NeighborhoodCardTopContent>
 			<NeighborhoodCardBottomContent>
-				<Title>Test title</Title>
+				<Title>{neighborhood.title}</Title>
 				<Description margin='0'>Description</Description>
 			</NeighborhoodCardBottomContent>
 		</NeighborhoodCardWrapper>
--- server/src/components/app/app.module.ts
+++ server/src/components/app/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '../auth/auth.module';
+import { NeighborhoodsModule } from '../neighborhoods/neighborhoods.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from '../auth/auth.module';
       'mongodb+srv://admin:admin@users.fdv5b.mongodb.net/neighborhood',
     ),
     AuthModule,
+	  NeighborhoodsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
